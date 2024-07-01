fcefda90b42b321924222fb7b7c38c4a6c5a2c54
Ngo Van Toan <toante2000@gmail.com>
2024-07-01 12:32:08 +0700

    push ip

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     // Định nghĩa URL API của bạn ở đây
-    API_URL: 'http://157.173.218.3/antraveling/api',
+    API_URL: 'http://127.0.0.1/antraveling/api',
   },
 }
 
